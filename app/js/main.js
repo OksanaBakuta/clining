b9193710faa88e07738a465ccccf04b7598750a4
OksanaBakuta <oksanabakuta@gmail.com>
2020-12-24 20:06:12 +0200

    added section swrvices

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,10 @@
 $(function(){
 
   var show = true;
-  var countbox = ".info__items";
   $(window).on("scroll load resize", function () {
-    if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-    var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-    var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-    var w_height = $(window).height(); // Высота окна браузера
-    var d_height = $(document).height(); // Высота всего документа
-    var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-    if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-      $('.info__item-num').css('opacity', '1');
+    if (!show) return false;
+    var w_top = $(window).scrollTop();
+    if (w_top >= 100) {
       $('.info__item-num').each(function () {
         $(this).prop('Counter', 0).animate({
           Counter: $(this).text()
@@ -26,7 +20,5 @@ $(function(){
       show = false;
     }
   });
-
-
 
 });
