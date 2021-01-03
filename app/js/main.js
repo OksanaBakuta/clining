ab54806efacfa30e24a80737a49ed7eaf5e88643
OksanaBakuta <oksanabakuta@gmail.com>
2021-01-03 17:11:46 +0200

    added fixed menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
 $(function(){
 
+  $('.menu__link').click(function (e) {
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if (target.length) {
+      var scrollTo = target.offset().top;
+      $('body, html').animate({ scrollTop: scrollTo + 'px' }, 500);
+    }
+  });
+
+  var hh = $('.header').height();
+  $(window).scroll(function(){
+    if($(document).scrollTop() > hh){
+      $('.header-top__inner').addClass('header-top__inner--fixed');
+    }else{
+      $('.header-top__inner').removeClass('header-top__inner--fixed');
+    }
+  });
+
   $('.tabs__btn').on('click', function(e){
     e.preventDefault();
     $('.tabs__btn').removeClass('tabs__btn--active');
